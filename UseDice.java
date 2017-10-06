d0d9dcfa50c9a46e81df57533c75bafbf27e97e2
Snowspect <s155384@student.dtu.dk>
2017-10-06 10:13:13 +0200

    comment added
    Only comments added

--- UseDice.java
+++ UseDice.java
@@ -19,12 +19,13 @@ package terning;
 import java.util.Scanner;
 
 public class UseDice {
-
+	
 	public static void main(String[] args) {
 		Dice cup = new Dice();   // Make an instance of Dice
 		Scanner keyboard=new Scanner(System.in);   // Make an instance of keyboard Scanner
 		System.out.println("Welcome to the diceroll program");
 		System.out.println("How many times do you want to roll the dice?");
+		//scans only for integers, if other input, then exception.
 		int n = keyboard.nextInt();  // Read integer
 		cup.rollMultiple(n);         // Roll the dice n times
 		keyboard.close();            // Close the scanner
